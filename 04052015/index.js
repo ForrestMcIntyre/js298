191bc8c62a0339f5f6d01397e810a5c5065b1868
Forrest McIntyre <fmcint02@seattlecentral.edu>
2015-05-07 01:08:34 -0700

    Hapi.js portfolio files added
    Added the files for the hapi.js mock portfolio that we made in class
    during the lecture on 04-05-2015

---
+++ 04052015/index.js
@@ -0,0 +1,55 @@
+var hapi = require("hapi");
+var fs = require("fs");
+var server = new hapi.Server();
+
+server.connection({
+  port:8000
+});
+
+server.start();
+
+
+
+server.views({
+  path: "templates",
+  engines: {
+    html: require("handlebars"),
+  },
+  layoutPath: "layouts",
+  layout: "default",
+  partialsPath:"templates/partials",
+  isCached: false
+});
+
+server.route({
+  method: "GET",
+  path: "/projects",
+  handler: function(req, reply){
+    fs.readFile("classes.json", "utf8", function(err, data){
+      reply.view("classes", {
+        title: "Classes",
+        classes: JSON.parse(data)
+      });
+    })
+  }
+})
+
+server.route({
+  method: "GET",
+  path: "/",
+  handler: function(req, reply){
+    reply.view("index", {
+      title: "Home"
+    });
+  }
+});
+
+server.route({
+  method: "GET",
+  path: "/assets/{param*}",
+  handler: {
+    directory: {
+      path: "public"
+    }
+  }
+});
